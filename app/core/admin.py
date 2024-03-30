4263e8f3b1775fc73e05549f31eb7a2970860420
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-03-30 16:18:23 +0200

    feat(admin): customize django admin for user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,18 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for the users."""
+
+    ordering = ["id"]
+    list_display = ["email", "first_name", "last_name"]
+
+
+admin.site.register(models.User, UserAdmin)
