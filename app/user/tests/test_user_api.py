f3f6a323780a38b6e04bee7d4859d05e6d645d6b
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-01 00:19:36 +0300

    test(user): add jwt token tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,8 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
+JWT_TOKEN_URL = reverse("user:obtain-token-pair")
+JWT_TOKEN_REFRESH_URL = reverse("user:token-refresh")
 
 
 def create_user(**params):
@@ -59,3 +61,78 @@ class PublicUserApiTests(TestCase):
             .exists()
         )
         self.assertFalse(user_exists)
+
+    def test_obtain_token_for_user(self):
+        """Test obtain token pair for valid credentials."""
+        user_details = {
+            "first_name": "First",
+            "last_name": "Last",
+            "email": "user@example.com",
+            "password": "testpass123",
+        }
+        create_user(**user_details)
+
+        payload = {
+            "email": user_details["email"],
+            "password": user_details["password"],
+        }
+        res = self.client.post(JWT_TOKEN_URL, payload)
+
+        self.assertIn("refresh", res.data)
+        self.assertIn("access", res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_obtain_token_bad_credentials(self):
+        """Test return error if credentials is invalid."""
+        create_user(email="test@example.com", password="testpass123")
+
+        payload = {"email": "test@example.com", "password": "testpass321"}
+
+        res = self.client.post(JWT_TOKEN_URL, payload)
+
+        self.assertNotIn("refresh", res.data)
+        self.assertNotIn("access", res.data)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_obtain_token_blank_password(self):
+        """Test posting a blank password return an error."""
+        payload = {"email": "test@example.com", "password": ""}
+
+        res = self.client.post(JWT_TOKEN_URL, payload)
+
+        self.assertNotIn("refresh", res.data)
+        self.assertNotIn("access", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_refresh_token(self):
+        """Test refreshing a token."""
+
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123",
+        }
+        create_user(
+            email=payload["email"],
+            password=payload["password"],
+        )
+
+        # Obtain token
+        obtain_res = self.client.post(JWT_TOKEN_URL, payload)
+
+        refresh_token = obtain_res.data["refresh"]
+
+        refresh_payload = {"refresh": refresh_token}
+
+        # Refresh token
+        refresh_res = self.client.post(JWT_TOKEN_REFRESH_URL, refresh_payload)
+
+        self.assertIn("access", refresh_res.data)
+        self.assertNotEqual(
+            obtain_res.data["refresh"],
+            refresh_res.data["refresh"],
+        )
+        self.assertNotEqual(
+            obtain_res.data["access"],
+            refresh_res.data["access"],
+        )
+        self.assertEqual(refresh_res.status_code, status.HTTP_200_OK)
