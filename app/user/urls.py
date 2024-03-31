0a5f67f9ac591e313cead4c52316b1e6be225a6f
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-01 00:22:54 +0300

    build: integrate drf-simplejwt for authentication

--- app/user/urls.py
+++ app/user/urls.py
@@ -6,8 +6,23 @@ from django.urls import path
 
 from user import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = "user"
 
 urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create"),
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="obtain-token-pair",
+    ),
+    path(
+        "api/token/refresh",
+        TokenRefreshView.as_view(),
+        name="token-refresh",
+    ),
 ]
