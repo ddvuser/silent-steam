8cb0148c134d2b1e9282799cdaf6f8a3d3de35e8
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-04 01:10:06 +0300

    test(models): add teacher and course model tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,8 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_success(self):
@@ -46,3 +48,39 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_teacher(self):
+        """test creating a teacher."""
+        user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="testpass123",
+            first_name="First",
+            last_name="Last",
+        )
+
+        teacher = models.Teacher.objects.create(
+            user=user,
+            degree="Degree",
+        )
+
+        self.assertEqual(str(teacher), f"{user.first_name} {user.last_name}")
+
+    def test_create_course(self):
+        """Test creating a course."""
+        user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="testpass123",
+            first_name="First",
+            last_name="Last",
+        )
+
+        teacher = models.Teacher.objects.create(
+            user=user,
+            degree="Degree",
+        )
+
+        course = models.Course.objects.create(
+            author=teacher, name="Course", description="Course Description"
+        )
+
+        self.assertEqual(str(course), course.name)
