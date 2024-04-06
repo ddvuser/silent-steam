feb82119829caf84c6703d22bf7bdafafb7a6dfb
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-06 16:09:10 +0300

    test(user api): add create_user_without_role test
    Adjust other tests for creating a user to include one more field - role

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,6 +32,7 @@ class PublicUserApiTests(TestCase):
             "password": "testpass123",
             "first_name": "First",
             "last_name": "Last",
+            "is_student": True,
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -45,6 +46,31 @@ class PublicUserApiTests(TestCase):
         payload = {
             "email": "test@example.com",
             "password": "test123",
+            "first_name": "First",
+            "last_name": "Last",
+            "is_student": True,
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = (
+            get_user_model()
+            .objects.filter(
+                email=payload["email"],
+            )
+            .exists()
+        )
+        self.assertFalse(user_exists)
+
+    def test_create_user_without_role(self):
+        """
+        Test creating a user without providing is_student or is_teacher.
+        """
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123",
             "first_name": "First",
             "last_name": "Last",
         }
