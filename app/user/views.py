963f791838aed42866fc653b5dcecc133a730e48
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-06 16:30:04 +0300

    feat(views): overwrite post method create user view to handle role fields errors

--- app/user/views.py
+++ app/user/views.py
@@ -3,6 +3,8 @@ Views for the user API.
 """
 
 from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
@@ -12,6 +14,24 @@ class CreateUserView(generics.CreateAPIView):
     """Create new user API View."""
 
     serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        is_teacher = request.data.get("is_teacher", False)
+        is_student = request.data.get("is_student", False)
+
+        if is_teacher and is_student:
+            return Response(
+                {"error": "User cannot be both a teacher and a student."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if not is_teacher and not is_student:
+            return Response(
+                {"error": "You must choose between student and teacher."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return super().post(request, *args, **kwargs)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
