513b532710f3609dabc4b5d4ebd07cb6e1ad2f07
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-04-08 01:22:26 +0300

    feat(course): create course viewset and course api endpoints

--- app/course/views.py
+++ app/course/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+"""
+Views for the courses APIs.
+"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
+from core.models import Course, Teacher
+from course import serializers
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """View for manage course API."""
+
+    serializer_class = serializers.CourseSerializer
+    queryset = Course.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve courses created by authenticated teacher."""
+        try:
+            teacher = Teacher.objects.get(user=self.request.user)
+            return self.queryset.filter(author=teacher).order_by("-id")
+        except Teacher.DoesNotExist:
+            # If user is a student, raise permission denied
+            raise PermissionDenied(
+                "\
+                Access denied: Only teachers can access this view.\
+                "
+            )
