48955792bf0ba47a275e97f9e64bb3c98cd213db
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-09-10 16:18:57 +0300

    feat(urls): include classroom urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
         name="api-docs",
     ),
     path("api/user/", include("user.urls")),
-    path("api/course/", include("course.urls")),
+    path("api/", include("classroom.urls")),
+    path("api/", include("course.urls")),
 ]
