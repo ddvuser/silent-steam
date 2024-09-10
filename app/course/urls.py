debdd0e6d9b9f8ea31e086dba3f6a70d1b6a5018
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-09-10 16:17:33 +0300

    fix(course): fix typo in urls

--- app/course/urls.py
+++ app/course/urls.py
@@ -12,7 +12,7 @@ from rest_framework.routers import DefaultRouter
 from course import views
 
 router = DefaultRouter()
-router.register("courses", views.CourseViewSet)
+router.register("course", views.CourseViewSet)
 
 app_name = "course"
 
